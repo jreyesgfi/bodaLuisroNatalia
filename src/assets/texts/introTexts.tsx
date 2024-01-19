1116290e4bc56ed6e57f131453aa8d927f5bdbd1
jorge <91345902+jreyesgfi@users.noreply.github.com>
2024-01-19 04:07:54 +0200

    Intro flow updated 1

---
+++ src/assets/texts/introTexts.tsx
@@ -0,0 +1,7 @@
+export const introText_1_1 = '¡Ey, hola! Si estas por aquí es porque te habrás enterado del gran salto que vamos a dar en nuestra vida, y es que…';
+export const introText_1_2 = '¡Nos casamos!';
+export const introText_2 = 'Hace ya 6 años, comenzamos a recorrer juntos un camino que nos ha hecho crecer, conocernos y aprender a querernos cada día más.';
+export const introText_3 = 'En este viaje hemos pasado diferentes etapas y momentos que nos han traído hasta aquí, y nos  han llevado a cruzarnos con personas muy especiales, que han sido luz y guía para nuestros pasos: nuestras “estrellitas".';
+export const introText_4 = 'Ellas, sin duda, han sido compañeras de peregrinaje, compartiendo momentos y enseñanzas en algún tramo del camino. ¡Y tú, eres una de ellas!';
+export const introText_5 = 'Queremos agradecerte tu paso por nuestra historia y decirte que nos haría mucha ilusión compartir nuestro día contigo. Ponte las botas y coge tu mochila… queremos que seas testigo del inicio de esta nueva etapa de nuestro camino.';
+export const introText_6 = '¿Te apuntas, peregrino?'
--- src/pages/Intro/IntroPage.tsx
+++ src/pages/Intro/IntroPage.tsx
@@ -9,74 +9,157 @@ import { IntroSection } from './sections/IntroSection';
 import { IntroSection2 } from './sections/IntroSection2';
 import { CustomButton } from '../../theme/components/Button';
 import { useCustomNavigate } from '../../theme/customHooks/useCustomNavigate';
+import { Section, Text } from '../../theme/globalStyles';
+import { introText_1_1, introText_1_2, introText_2, introText_3, introText_4, introText_5, introText_6 } from '../../assets/texts/introTexts';
+import { adjustUrlForEnvironment } from '../../serverConfig';
 
 
 const GlobalWrapper = styled.div`
   overflow: hidden;
   height: 98%;
-  width: 98vw;
+  width: 100%;
   padding: 0;
   margin: 0;
   scroll: none;
   position: relative;
   transition: opacity 0.5s ease; /* Added transition for opacity */
 `;
-
-// intialize the context
-export const IntroPage: React.FC= () => {
-
-  // States
-  //const [stage, setStage] = useState<number>([]);
-
-  const [currentSection, setCurrentSection] = useState<number>(0);
-  const customNavigate = useCustomNavigate();
-  // const handleNext = () => {
-  //   if (currentSection >=1) {
-  //     const groupID = searchParams.get('groupID');
-  //     const token = searchParams.get('token');
-  //     navigate(`/asistencia?groupID=${groupID}&token=${token}`);
-  //   }
-  //   setCurrentSection(currentSection + 1);
-  // }
-
-  const HistoryButton = styled(CustomButton)`
-    position: absolute;
-    bottom: 24px;
-    left: 0;
-    right: 0;
-    margin: 0 auto;
-    max-width: 200px;
-    background-color: #ffffff00;
-    z-index: 100;
-    
+const ScrollWrapper = styled.div`
+width: 100%;
+height: 100%;
+overflow: scroll;
+scroll-snap-type: y mandatory;
+`;
+const SemiSection = styled(Section)`
+height: 90%;
+border: 1px solid red;
+overflow: visible;
 `;
 
+const WrapperText = styled(Text)`
+text-align: center;
+position: absolute;
+inset: 0;
+margin: auto;
+max-height: fit-content;
+z-index:100;
+`;
+const RelativeWrapperText = styled(Text)`
+text-align: left;
+margin-top: 36px;
+z-index:100;
+position:relative;
+`;
+const BottomWrapperText = styled(WrapperText)`
+inset: 48px;
+top: auto;
+`;
+
+const BackgroundImage = styled.img`
+  width: 100vw;
+  height: 95%;
+  position:absolute;
+  inset: 0;
+  object-fit: cover;
+`;
+const RelativeImage = styled.img`
+  margin-left: -24px;
+  width: 100vw;
+  height: fit-content;
+  object-fit: contain;
+`;
+const BigRelativeImage = styled.img`
+  margin-left: -15vw;
+  width: 120vw;
+  height: fit-content;
+  object-fit: contain;
+`;
+
+const GradientDiv1 = styled.div`
+
+  height: 300px;
+  width: 100vw;
+  max-width: none;
+  top:0;
+  margin-top: -280px;
+  margin-left:-24px;
+  background: linear-gradient(to top, rgba(249, 248, 237, 1) 5%, rgba(249, 248, 237,0) 85%);;
+  `
+const GradientDiv2 = styled(GradientDiv1)`
+  position:absolute;
+  margin-top: -8px;
+  transform: rotate(180deg);
+  z-index:20;
+  `
+const RelativeHolder = styled.div`
+position: relative;
+width: 100%;
+`;
+
+const Button = styled(CustomButton)`
+  margin: 24px auto;
+  max-width: 200px;
+  z-index: 100;
+  font-size: 18px;
+`;
+// intialize the context
+export const IntroPage: React.FC = () => {
+const customNavigate = useCustomNavigate();
 const handleNext = () => {
-  
-  if (currentSection >= 1) {
-    customNavigate('asistencia');
-  }
-  else{
-    setCurrentSection(currentSection + 1);
-  }
-  
+  customNavigate('asistencia');
 };
 
-return (
-  <GlobalWrapper>
-    <SectionsWrapper
-      currentSection={currentSection}
-      sections={[
-        <IntroSection key={1} />,
-        <IntroSection2 key={2} />
-      ]}
-    />
-    <HistoryButton
-      onClick={handleNext}
-      highlighted={true}
-    >
-      Siguiente
-    </HistoryButton>
-  </GlobalWrapper>
-);
+  return (
+    <GlobalWrapper>
+      <ScrollWrapper>
+        <SemiSection inverse>
+          <BottomWrapperText inverse>
+            {introText_1_1}
+            <br /><br />
+            <b>{introText_1_2}</b>
+          </BottomWrapperText>
+          <BackgroundImage src={adjustUrlForEnvironment("../../../../public/assets/images/ApuntesCartaAcuarelaMarron.svg")} />
+        </SemiSection>
+        <SemiSection inverse>
+          <GradientDiv1 />
+          <RelativeImage src={adjustUrlForEnvironment("../../../../public/assets/images/Camino2.png")} />
+          <RelativeHolder>
+            <GradientDiv2 />
+            <RelativeWrapperText inverse>
+              {introText_2}
+            </RelativeWrapperText>
+          </RelativeHolder>
+        </SemiSection>
+        <SemiSection inverse>
+          <RelativeImage src={adjustUrlForEnvironment("../../../../public/assets/images/mirando-estrellas-silueta.png")} />
+          <RelativeWrapperText inverse>
+            {introText_3}
+          </RelativeWrapperText>
+        </SemiSection>
+        <SemiSection inverse>
+          <WrapperText inverse>
+            {introText_4}
+          </WrapperText>
+        </SemiSection>
+        <SemiSection inverse>
+          <RelativeWrapperText inverse>
+            {introText_5}
+          </RelativeWrapperText>
+        <RelativeImage src={adjustUrlForEnvironment("../../../../public/assets/images/mochila-botas.png")} /> 
+        </SemiSection>
+        <SemiSection inverse>
+        <BigRelativeImage src={adjustUrlForEnvironment("../../../../public/assets/images/piedra-te-apuntas.png")} />
+          <RelativeWrapperText inverse>
+            {introText_6}
+          </RelativeWrapperText>
+          <Button
+            onClick={handleNext}
+            selected={true}
+          >
+            <b>¡Estoy Listo!</b>
+          </Button>
+        </SemiSection>
+      </ScrollWrapper>
+    </GlobalWrapper>
+  );
 };
